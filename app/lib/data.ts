89a0c5031c8c0d973aa27ee3fa4a774d388e2173
Sunil N <47171992+snarhe@users.noreply.github.com>
2025-09-29 21:43:44 +0530

    chapter 11. Seach completed | data.ts sslmode

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,7 +9,7 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, {ssl: false});
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 export async function fetchRevenue() {
   try {
